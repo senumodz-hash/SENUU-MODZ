f322a864bb604aab4f149bb1b0e6a4d1b687ba2b
senumodz-hash <senumodz@gmail.com>
2026-01-13 13:23:04 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from './components/Hero'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Projects />
+      <Contact />
+    </>
+  )
+}
